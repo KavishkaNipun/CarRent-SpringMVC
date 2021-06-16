5a1ff38a59b8588739e2d2e46298ba95f4a2efbc
Kavishka Nipun <kavishkanipunofficial@gmail.com>
2021-06-16 10:54:52 -0700

    add property file and do some changes

--- src/main/java/lk/carrent/spring/config/WebAppConfig.java
+++ src/main/java/lk/carrent/spring/config/WebAppConfig.java
@@ -1,4 +1,11 @@
 package lk.carrent.spring.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "lk.carrent.spring")
 public class WebAppConfig {
 }
